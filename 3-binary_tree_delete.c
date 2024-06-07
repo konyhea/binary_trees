a90417952b0046efe3a2f089565dee0ce94cccca
Nduka Konyhea <ndukaprincek@gmail.com>
2024-06-07 08:59:49 -0700

    fixing errors

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,15 +1,18 @@
 #include "binary_trees.h"
 
-/* delete an entire tree */
+/**
+ * binary_tree_delete - delete thw whole tree
+ * @tree: tree to be removed
+ *
+ * Return (void)
+ *
+ */
 
 void binary_tree_delete(binary_tree_t *tree)
 {
-        if (tree == NULL)
-        {
-                return;
-        }
-
-        binary_tree_delete(tree->right);
-        binary_tree_delete(tree->left);
-        free(tree);
+	if (tree == NULL)
+		return;
+	binary_tree_delete(tree->right);
+	binary_tree_delete(tree->left);
+	free(tree);
 }
--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -1,13 +1,18 @@
 #include "binary_trees.h"
 
-/* is it root */
+/**
+ * binary_tree_is_root - is it root
+ * @node: node to check it's root status
+ *
+ * Return: Always (0)
+ */
+
 int binary_tree_is_root(const binary_tree_t *node)
 {
-        if (node == NULL)
-           return 0;
-
-        if (node->parent == NULL)
-            return 1;
-        else
-            return 0;
+	if (node == NULL)
+		return (0);
+	if (node->parent == NULL)
+		return (1);
+	else
+		return (0);
 }
