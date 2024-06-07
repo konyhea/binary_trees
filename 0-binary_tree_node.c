2a6a06629c4935cc41b059dc74131d7baa02b294
Nduka Konyhea <ndukaprincek@gmail.com>
2024-06-06 21:37:46 -0700

    fix

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -3,18 +3,22 @@
 
 /* Function that creates a binary tree node */
 
+/**
+ * binary_tree_t *binary_tree_node - create a binary tree
+ * @parent: pointer to the parent node
+ * @value: is the value to put in the node
+ *
+ * Return: Always 0
+ */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t *newnode;
+	binary_tree_t *newnode;
 
-    newnode = (binary_tree_t *)malloc(sizeof(binary_tree_t));
-    if (newnode == NULL)
-        return NULL;
-
-    newnode->n = value;
-    newnode->parent = parent;
-    newnode->left = NULL;
-    newnode->right = NULL;
-
-    return newnode;
-}
+	newnode = (binary_tree_t *)malloc(sizeof(binary_tree_t));
+	if (newnode == NULL)
+		return (NULL);
+	newnode->n = value;
+	newnode->parent = parent;
+	newnode->left = NULL;
+	newnode->right = NULL;
+	return (newnode);
--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -4,14 +4,13 @@
 /* function to insert a left node */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-    if (parent == NULL)
-        return NULL;
-
-    binary_tree_t *newnode = malloc(sizeof(binary_tree_t));
-    if (newnode == NULL)
-        return NULL;
-
-    newnode->n = value;
+	if (parent == NULL)
+		return (NULL);
+	
+	binary_tree_t *newnode = malloc(sizeof(binary_tree_t));
+	if (newnode == NULL)
+		return (NULL);
+   	newnode->n = value;
     newnode->parent = parent;
     newnode->left = parent->left; // The old left child becomes the left child of the new node
     newnode->right = NULL;
@@ -21,5 +20,5 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 
     parent->left = newnode; // The new node becomes the left child of the parent
 
-    return newnode;
+    return (newnode;
 }
