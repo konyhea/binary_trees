920038a0e0db830bb6947b10e35039b05dde1c99
Nduka Konyhea <ndukaprincek@gmail.com>
2024-06-07 09:02:28 -0700

    404 errors

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,24 +1,31 @@
 #include "binary_trees.h"
 #include <stdlib.h>
+/**
+ * binary_tree_t *binary_tree_insert_left  - function to add to left of a node
+ * @parent: the node adding to the left
+ * @value: the data value in the left node
+ *
+ * Return: 0 (success)
+ */
 
-/* function to insert a left node */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	if (parent == NULL)
 		return (NULL);
-	
+
 	binary_tree_t *newnode = malloc(sizeof(binary_tree_t));
+
 	if (newnode == NULL)
 		return (NULL);
-   	newnode->n = value;
-    newnode->parent = parent;
-    newnode->left = parent->left; // The old left child becomes the left child of the new node
-    newnode->right = NULL;
+	newnode->n = value;
+	newnode->parent = parent;
+	newnode->left = parent->left;
+	newnode->right = NULL;
 
-    if (parent->left != NULL)
-        parent->left->parent = newnode; // Update the parent of the old left child
+	if (parent->left != NULL)
+		parent->left->parent = newnode;
 
-    parent->left = newnode; // The new node becomes the left child of the parent
+	parent->left = newnode;
 
-    return (newnode;
+	return (newnode);
 }
--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -1,13 +1,19 @@
 #include "binary_trees.h"
 
-/* Do you have any child */
+/**
+ * binary_tree_is_leaf - is it a leaf
+ * @node: node to be checked
+ *
+ * Return: Always (0)Always
+ */
+
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-        if (node == NULL)
-            return 0;
+	if (node == NULL)
+		return (0);
 
-        if (node->left == NULL &&  node->right == NULL)
-           return 1;
-        else
-            return 0;
+	if (node->left == NULL && node->right == NULL)
+		return (1);
+	else
+		return (0);
 }
