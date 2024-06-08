bd1cc23bbc4b2ab5dd83e25c5317e5c432615d56
Nduka Konyhea <ndukaprincek@gmail.com>
2024-06-07 21:53:36 -0700

    leaves for leaf

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,16 +1,17 @@
 #include "binary_trees.h"
 
-/* Function to count the leaves in a binary tree */
+/**
+ * binary_tree_leaves - func to count number of leaves in a tree
+ * @tree: tree to be travered
+ *
+ * Return: Always 0 (success)
+ *
+ */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-    if (tree == NULL) {
-        return 0;  // Return 0 if the tree is NULL
-    }
-
-    if (tree->left == NULL && tree->right == NULL) {
-        return 1;  // Return 1 if the node is a leaf
-    }
-
-    // Recursively count the leaves in the left and right subtrees
-    return binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right);
+	if (tree == NULL)
+		return (0);
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
 }
