91c5bc0e018154436eace534c6be6be416cb974e
Nduka Konyhea <ndukaprincek@gmail.com>
2024-06-07 21:59:40 -0700

    ah sze

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,13 +1,16 @@
 #include "binary_trees.h"
-
+/**
+ * binary_tree_size - return size of a binary tree
+ * @tree: tre to be calculated
+ *
+ *
+ * Return: Always 0
+ */
 /* Function to measure the size of a binary tree */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-    if (tree == NULL)
-    {
-        return 0; // Return 0 if the tree is NULL
-    }
-
-    // Return the size of the tree, which is 1 (for the current node) plus the sizes of left and right subtrees
-    return 1 + binary_tree_size(tree->left) + binary_tree_size(tree->right);
+	if (tree == NULL)
+		return (0);
+	return (1 + (binary_tree_size(tree->left)
+				+ binary_tree_size(tree->right)));
 }
