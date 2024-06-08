4caa18ba347437ea3c889d5399eac98fa20dec64
Nduka Konyhea <ndukaprincek@gmail.com>
2024-06-07 18:32:24 -0700

    6 flex

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -1,12 +1,20 @@
 #include "binary_trees.h"
 
-/* function to pre-orderf traversal */
+/**
+ * binary_tree_preorder - traverse pre-orderly
+ *
+ * @tree: the tree to traverse
+ * @func: function to a pointer
+ *
+ * Return
+ *
+ */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
-        if (tree == NULL || func == NULL)
-            return;
+	if (tree == NULL || func == NULL)
+		return;
 
-        func(tree->n);
-        binary_tree_preorder(tree->left, func);
-        binary_tree_preorder(tree->right, func);
+	func(tree->n);
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
 }
